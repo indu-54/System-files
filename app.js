5c6cb96a5873b4bd56b47ffd1a45c75404b5189d
indu-54 <150770818+indu-54@users.noreply.github.com>
2024-02-05 18:31:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,8 @@ app.use(function (req, res, next) {
 
 //MongoDB Atlas connection using native driver
 // const MongoClient = require('mongodb').MongoClient;
-const uri = 'mongodb+srv://indusunkari7:yYbhXqWkTYLK6Dr6@cluster0.jbspbuv.mongodb.net/?retryWrites=true&w=majority';
+const uri = 'const uri = 'mongodb+srv://indusunkari7:FR5ajziZ3Qu4vAa6@cluster0.pszusn4.mongodb.net/?retryWrites=true&w=majority';
+';
 const client = new MongoClient(uri, {});
 
 //Connect to MongoDB Atlas
